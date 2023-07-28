33a2c98323596f983b25ef648118889d17bf946c
tduncan14 <nbatre1@yahoo.com>
2023-07-27 21:10:56 -0700

    keep moving forward

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -7,12 +7,32 @@ import Checkoutsteps from '../Components/CheckoutSteps';
 
 const PlaceOrderScreen = () => {
 
+    const navigate = useNavigate();
+    const cart = useSelector ((state) => state.cart)
 
   
+    useEffect(() => {
+
+        if(!cart.shippingAddress.address){
+            navigate('/shipping')
+        }
+        else if(!cart.paymentMethod){
+            navigate('/payment')
+        }
+
+    },[cart.paymentMethod, cart.shippingAddress.address, navigate])
+
+
     return(
-        <div>
-            Screen
-        </div>
+       <>
+       <Checkoutsteps step1 step2 step3 step4 />
+
+       <Row>
+        <Col md={8}> Column</Col>
+        <Col md={4}> Column</Col>
+
+       </Row>
+       </>
     )
 
 
